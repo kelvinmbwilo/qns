9105d4d0fbd2fbab8708f15f779edf1c199f6383
kelvinmbwilo <kelvinmbwilo@gmail.com>
2014-07-23 10:34:04 +0300

    my commit

--- kigoda/app/views/activities.blade.php
+++ kigoda/app/views/activities.blade.php
@@ -85,6 +85,376 @@
             </div>
         </div>
 <hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">LAUNCHING OF THE MWALIMU JULIUS NYERERE PROFESSORIAL CHAIR IN PAN AFRICAN STUDIES</h4>
+
+                <h5>Theme</h5>
+                <p>“African nationalism is meaningless, is anachronistic, and is dangerous, If is not at the same time Pan-Africanism”) (J.K. Nyerere: 1967</p>
+                <span class="pull-left"><b><em>Nkrumah Hall</em></b></span>
+                <span class="pull-right"><b><em>19th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">PUBLIC LECTURE ON DARFUR  By Prof Mahmood Mamdani</h4>
+
+                <p><b>Theme: </b>The public lecture on Darfur: The problem and the way forward</p>
+                <span class="pull-left"><b><em>Nkrumah Hall</em></b></span>
+                <span class="pull-right"><b><em>19th August 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">MBONGI WA PILI WA  MAKAMU MKUU WA CHUO KIKUU</h4>
+
+                <p><b>Theme: </b>Zahma za chakula na mafuta (Food and fuel crisis)</p>
+                <span class="pull-left"><b><em>Nkrumah Hall</em></b></span>
+                <span class="pull-right"><b><em>10th October 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">JULIUS NYERERE INTELLECTUAL FESTIVAL WEEK, Distinguished Nyerere Lecturer: Prof. Wole Soyinka</h4>
+
+           <strong>Theme: </strong>     A week of reflection on Pan-Africanism
+<br>
+                Binadamu wote ni sawa<br>
+                Afrika ni moja<br>
+                Africa must unite
+                <span class="pull-left"><b><em>Nkrumah Hall</em></b></span>
+                <span class="pull-right"><b><em>April 13th  – 17th 2009</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">VICE-CHANCELLOR’S PALAVER</h4>
+
+                <p><strong>Theme: </strong>Crisis of International System & African’s Development</p>
+                <span class="pull-left"><b><em>Nkrumah Hall</em></b></span>
+                <span class="pull-right"><b><em>16th April 2009</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">OPEN SEMINAR<br>
+ Mwalimu Nyerere Chair in collaboration with UDASA and CODESRIA</h4>
+
+                Remembrance of Tajudeen Abdul Raheem and Chachage Night<br>
+                <b>Topic:</b><br>
+                Tajudeen: A Pan-Africanist
+                Reminiscences<br>
+                Tribute to Prof. Haroub Othman
+                Chachage Night
+                Chachage-an active Codesrian
+                2nd Chachage lecture
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>July 10th  2009</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
+<div class="media">
+            <a class="pull-left" href="#">
+                <img style="width: 64px;height: 64px" src="{{ asset("img/image1.png") }}" class="img-rounded" />
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Launching Of Two Books</h4>
+
+                <h5>Books Lunched</h5>
+                1. Review of In Search of Relevance: A History of the University of Dar es Salaam. Edited by Isaria Kimambo, Bertram Mapunda and Yusuph Lawi<br><br>
+
+                2. Review of Pan-Africanism or Pragmatism? Lessons of Tanganyika-Zanzibar Union. By Issa Shivji<br><br>
+                <span class="pull-left"><b><em>Council Chamber</em></b></span>
+                <span class="pull-right"><b><em>20th April 2008</em></b></span>
+            </div>
+        </div>
+<hr>
+
         </div>
     </div>
 @stop
